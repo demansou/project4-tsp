fe2cd0c06d79f319375f2ce24710f187da8935ac
demansou <demansou@gmail.com>
2016-06-04 01:21:59 -0500

    testing optimizehamcycle (workingish)

--- main.py
+++ main.py
@@ -287,7 +287,7 @@ def main(filepath):
     map = formatmap(filecontents)
     hamcycle = greedyhamiltoniancycle(map)
     numloops = 2 * math.ceil(math.log10(len(hamcycle)))
-    print("%f" % numloops)
+    numloops = int(numloops)
     if COMPETITION:
         numloops /= 2
         numloops = int(numloops)

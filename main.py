5d90069eafc25c1e88ae3d964dabb75d3dcd6f8f
demansou <demansou@gmail.com>
2016-06-04 00:27:51 -0500

    testing optimizehamcycle (workingish)

--- main.py
+++ main.py
@@ -20,9 +20,13 @@ import random
 # DEBUG
 # 1 = ON
 # 0 = OFF
+# COMPETITION
+# 1 = ON
+# 0 = OFF
 #############################################################
 
 DEBUG = 0
+COMPETITION = 1
 
 #############################################################
 # Classes
@@ -215,16 +219,13 @@ def optimizehamcycle(hamcycle):
     :param hamcycle:
     :return:
     """
-    improveperformance = 0
-    if len(hamcycle) > 2500:
-        improveperformance = 1
     for i in range(0, len(hamcycle) - 1):
         for j in range(0, len(hamcycle) - 1):
             # print("[optimizehamcycle] i: %d\tj: %d" % (i, j))
             if i != j:
                 if routes_overlap(hamcycle[i], hamcycle[j]):
                     hamcycle = edge_swap(hamcycle, i, j)
-                    if improveperformance:
+                    if COMPETITION:
                         randomnumber = random.randrange(0, 100)
                         if randomnumber == 0:
                             break
